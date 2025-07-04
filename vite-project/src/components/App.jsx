eba2ab8c1f8f8766a64ee6d4188e5627d1b36c05
anandavallis <sanandavalli7@gmail.com>
2025-07-04 11:24:57 +0530

    Create App.jsx

---
+++ vite-project/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FocusInput from './components/FocusInput';
+
+const App = () => {
+  return (
+    <div>
+      <h1>useRef Input Focus Example</h1>
+      <FocusInput />
+    </div>
+  );
+};
+
+export default App;
